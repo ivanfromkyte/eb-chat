1ea64ba11c4824c719d988e7975063791993b53a
ivanfromkyte <97263559+ivanfromkyte@users.noreply.github.com>
2022-07-07 20:40:24 -0300

    Update and rename translation_v5.js to translation_v6.js

--- translation_v6.js
+++ translation_v6.js
@@ -1,12 +1,12 @@
 function getBrowserLang() {
-  if (navigator.languages) return navigator.languages[0] || 'pt-BR';
-  if (navigator.language) return navigator.language || 'pt-BR';
-  return 'pt-BR';
+  if (navigator.languages) return navigator.languages[0] || 'pt-BR'
+  if (navigator.language) return navigator.language || 'pt-BR'
+  return 'pt-BR'
 }
 function convertBrowserLang(iso) {
-  if (iso.startsWith('pt')) return 'pt';
-  if (iso.startsWith('es')) return 'es';
-  return 'en';
+  if (iso.startsWith('pt')) return 'pt'
+  if (iso.startsWith('es')) return 'es'
+  return 'en'
 }
 const translations = {
   en: {
@@ -40,31 +40,22 @@ const translations = {
     message: 'Tu mensaje: cuanto más detallado y directo, mejor y más rápido podremos ayudarte',
   },
 };
-const replaceCurlyBraces = () => {
-  const isoLang = getBrowserLang();
-  const useLang = convertBrowserLang(isoLang.toLocaleLowerCase());
-  const values = translations[useLang];
-  if (values['greetingTitle'] !== undefined)
+
+document.addEventListener("DOMContentLoaded", function () {
+  const isoLang = getBrowserLang()
+  const useLang = convertBrowserLang(isoLang.toLocaleLowerCase())
+  const translationKeys = translations[useLang];
     document.getElementsByClassName('chat__prompt-message-copy')[0].textContent = values['greetingTitle'];
-  if (values['baseMessage'] !== undefined)
     document.getElementsByClassName('chat__prompt-base-helper')[0].textContent = values['baseMessage'];
-  if (values['Check'] !== undefined)
     document.getElementsByClassName('chat__prompt-input')[0].textContent = values['Check'];
-  if (values['newChat'] !== undefined)
     document.getElementsByClassName('chat__all-btn')[0].textContent = values['newChat'];
-  if (values['noChat'] !== undefined)
     document.getElementsByClassName('chat__all-messages-track')[0].textContent = values['noChat'];
-  if (values['formTitle'] !== undefined)
     document.getElementsByClassName('mb-2')[0].textContent = values['formTitle'];
-  if (values['startChat'] !== undefined)
     document.getElementsByClassName('chat__messages-btn')[0].textContent = values['startChat'];
-  if (values['name'] !== undefined)
     document.getElementsByName('name')[0].textContent = values['name'];
-  if (values['email'] !== undefined)
     document.getElementsByName('email')[0].textContent = values['email'];
-  if (values['issue'] !== undefined)
     document.getElementsByName('Issue')[0].textContent = values['issue'];
-  if (values['message'] !== undefined)
     document.getElementsByName('message')[0].textContent = values['message'];
-};
-document.addEventListener('DOMContentLoaded', replaceCurlyBraces);
+});
+
+document.addEventListener('DOMContentLoaded', setButtonLink);
